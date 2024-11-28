523375eddbefd64ab6c295bc3c710d873eaeb722
Ex-Rockstar <ruthirockstar2218@gmail.com>
2024-11-28 06:03:13 +0530

    changed caesar cipher

--- caeserCipher.java
+++ caeserCipher.java
@@ -6,7 +6,7 @@ class CaesarCipher {
         for (char i : enc.toCharArray()) {
             if (Character.isLetter(i)) {
                 if (Character.isUpperCase(i)) {
-                    encoded.append((char) ('A' + (i - 'A' + offset) % 26));
+                    encoded.append((char) ('A' + (i - 'A' + offset) % 26));                    
                 } else {
                     encoded.append((char) ('a' + (i - 'a' + offset) % 26));
                 }
